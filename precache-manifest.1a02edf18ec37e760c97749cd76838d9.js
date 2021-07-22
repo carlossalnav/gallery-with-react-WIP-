222c2252bc2741a21165a9eeb1179a01702eb08b
carlossalnav <carlos.salnav@gmail.com>
2021-07-22 16:14:20 -0500

    Updates

---
+++ precache-manifest.1a02edf18ec37e760c97749cd76838d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a03e976f70eb8c21e0b410187e7a9d0",
+    "url": "/gallery-with-react-WIP-/index.html"
+  },
+  {
+    "revision": "af353e68cf963baf4aea",
+    "url": "/gallery-with-react-WIP-/static/css/main.1d1668eb.chunk.css"
+  },
+  {
+    "revision": "9281c2be33290fca5e67",
+    "url": "/gallery-with-react-WIP-/static/js/2.54ace99e.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/gallery-with-react-WIP-/static/js/2.54ace99e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af353e68cf963baf4aea",
+    "url": "/gallery-with-react-WIP-/static/js/main.9d600b8d.chunk.js"
+  },
+  {
+    "revision": "f44df6601a6734283ef2",
+    "url": "/gallery-with-react-WIP-/static/js/runtime-main.ccd7a512.js"
+  }
+]);
